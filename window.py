c61ffbb8c241acf6807781c7adb29cb80f30b542
yhblue <1054635688@qq.com>
2017-08-10 22:17:36 +0800

    2017/8/10 evening

--- window.py
+++ window.py
@@ -1,14 +1,163 @@
 import pygame
+from pygame.locals import *
 import time
+import socket
+import threading
+
+PIC_X = 1000
+PIC_Y = 600
+
+START_X = 0
+START_Y = 0
+
+HERO_X = 40
+HERO_Y = 40
+
+ENEMY_X = 40
+ENEMY_Y = 40
+
+X_MAX = PIC_X - HERO_X   
+Y_MAX = PIC_Y - HERO_Y
+
+STEP = 2
+
+HERO_PIC_PATH = "./pic/hero.jpg"
+ENEMY_PIC_PATH = "./pic/enemy.jpg"
+BACK_PIC_PATH = "./pic/background.jpg"
+
+ADDRESS = "localhost"
+PORT=8888
+
+class HERO_PLAYER(object):
+	"""docstring for player"""
+	def __init__(self, screen):
+		self.x = 0
+		self.y = 0
+		self.screen = screen
+		self.image = pygame.image.load(HERO_PIC_PATH);
+		self.up = False
+		self.left = False
+		self.right = False
+		self.down = False
+
+	def display(self):
+		self.screen.blit(self.image, (self.x,self.y))
+
+	def move_left(self):
+		if self.x >= STEP:
+			self.x -= STEP
+
+	def move_right(self):			
+		if self.x <= X_MAX - STEP:
+			self.x += STEP
+
+	def move_up(self):
+		if self.y >= STEP:
+			self.y -= STEP
+
+	def move_down(self):
+		if self.y <= Y_MAX - STEP:
+			self.y += STEP
+
+	def position_update(self):
+		if(self.up == True):
+			self.move_up()
+		if(self.down == True):
+			self.move_down()
+		if(self.left == True):
+			self.move_left()
+		if(self.right == True):
+			self.move_right()	
+
+
+class ENEMY_PLAYER(object):
+	"""docstring for enemy"""
+	def __init__(self, screen):
+		self.x = 0
+		self.y = 0
+		self.screen = screen
+		self.image = pygame.image.load(ENEMY_PIC_PATH);
+
+	def display(self):
+		self.screen.blit(self.image, (self.x,self.y))
+
+	def move_left(self):
+		if self.x >= STEP:
+			self.x -= STEP
+
+	def move_right(self):			
+		if self.x <= X_MAX - STEP:
+			self.x += STEP
+
+	def move_up(self):
+		if self.y >= STEP:
+			self.y -= STEP
+
+	def move_down(self):
+		if self.y <= Y_MAX - STEP:
+			self.y += STEP
+
+
+def key_control(hero):
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			print("exit")
+			exit()
+
+		elif event.type ==  KEYDOWN:
+
+			if event.key == K_LEFT:
+				hero.left = True
+
+			elif event.key == K_RIGHT:
+				hero.right = True
+
+			elif event.key == K_UP:
+				hero.up = True
+
+			elif event.key == K_DOWN:
+				hero.down = True
+
+		elif event.type ==  KEYUP:
+			hero.down = False
+			hero.left = False			
+			hero.right = False
+			hero.up = False
+
+
+def client_send(hero):
+
+
+def client_recv():
+
+
 def main():
-	win = pygame.display.set_mode((1024,625),0,32)
-	background = pygame.image.load("./pic/background.jpg");
-    while True:
-        screen.blit(background, (0,0))
+	screen = pygame.display.set_mode((PIC_X,PIC_Y),0,32)
+	background = pygame.image.load(BACK_PIC_PATH);
+	hero = HERO_PLAYER(screen);
+	enemy = ENEMY_PLAYER(screen); 
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((ADDRESS, PORT)) 
 
-        pygame.display.update()
+	print('thread %s is running...' % threading.current_thread().name)
+	thread_read = threading.Thread(target=client_send, name='client_send_Thread')
+	thread_write = threading.Thread(target=client_recv,name='client_recv_Thread')
+	thread_read.start()
+	thread_write.start()
 
-        time.sleep(0.01)
+	while True:
+		screen.blit(background, (START_X,START_Y))
+		hero.display()
+		enemy.display();
+		pygame.display.update()
+
+		key_control(hero);
+		hero.position_update()
+		time.sleep(0.01)
+
+		thread_read.join()
+		thread_write.join()
 
 if __name__ == "__main__":
-    main()
+	main()
+
