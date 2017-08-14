5a9ad1d289f4a788e523133049f3672ce161b165
yhblue <1054635688@qq.com>
2017-08-14 22:26:58 +0800

    2017/8/14

--- message_pb2.py
+++ message_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='message.proto',
   package='',
-  serialized_pb='\n\rmessage.proto\":\n\x08Hero_msg\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\x0f\n\x07point_x\x18\x02 \x02(\x05\x12\x0f\n\x07point_y\x18\x03 \x02(\x05\"\x0c\n\nHeart_beat\"\x0b\n\tLogin_req\"\x0b\n\tLogin_rsp\"<\n\nNew_client\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\x0f\n\x07point_x\x18\x02 \x02(\x05\x12\x0f\n\x07point_y\x18\x03 \x02(\x05\";\n\tEnemy_msg\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\x0f\n\x07point_x\x18\x02 \x02(\x05\x12\x0f\n\x07point_y\x18\x03 \x02(\x05')
+  serialized_pb='\n\rmessage.proto\"9\n\x08Hero_msg\x12\x0b\n\x03uid\x18\x01 \x02(\x05\x12\x0f\n\x07point_x\x18\x02 \x02(\x05\x12\x0f\n\x07point_y\x18\x03 \x02(\x05\"\x0c\n\nHeart_beat\"\x19\n\tLogin_req\x12\x0c\n\x04name\x18\x01 \x02(\t\"^\n\tLogin_rsp\x12\x0f\n\x07success\x18\x01 \x02(\x08\x12\x0f\n\x07point_x\x18\x02 \x02(\x05\x12\x0f\n\x07point_y\x18\x03 \x02(\x05\x12\x11\n\tenemy_num\x18\x04 \x02(\x05\x12\x0b\n\x03uid\x18\x05 \x02(\x05\"\r\n\x0b\x43onnect_req\"\x1e\n\x0b\x43onnect_rsp\x12\x0f\n\x07success\x18\x01 \x02(\x08\";\n\tEnemy_msg\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\x0f\n\x07point_x\x18\x02 \x02(\x05\x12\x0f\n\x07point_y\x18\x03 \x02(\x05')
 
 
 
@@ -26,9 +26,9 @@ _HERO_MSG = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='name', full_name='Hero_msg.name', index=0,
-      number=1, type=9, cpp_type=9, label=2,
-      has_default_value=False, default_value=unicode("", "utf-8"),
+      name='uid', full_name='Hero_msg.uid', index=0,
+      number=1, type=5, cpp_type=1, label=2,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
@@ -56,7 +56,7 @@ _HERO_MSG = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=17,
-  serialized_end=75,
+  serialized_end=74,
 )
 
 
@@ -76,8 +76,8 @@ _HEART_BEAT = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=77,
-  serialized_end=89,
+  serialized_start=76,
+  serialized_end=88,
 )
 
 
@@ -88,6 +88,13 @@ _LOGIN_REQ = _descriptor.Descriptor(
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
+    _descriptor.FieldDescriptor(
+      name='name', full_name='Login_req.name', index=0,
+      number=1, type=9, cpp_type=9, label=2,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -97,8 +104,8 @@ _LOGIN_REQ = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=91,
-  serialized_end=102,
+  serialized_start=90,
+  serialized_end=115,
 )
 
 
@@ -109,44 +116,37 @@ _LOGIN_RSP = _descriptor.Descriptor(
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=104,
-  serialized_end=115,
-)
-
-
-_NEW_CLIENT = _descriptor.Descriptor(
-  name='New_client',
-  full_name='New_client',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
     _descriptor.FieldDescriptor(
-      name='name', full_name='New_client.name', index=0,
-      number=1, type=9, cpp_type=9, label=2,
-      has_default_value=False, default_value=unicode("", "utf-8"),
+      name='success', full_name='Login_rsp.success', index=0,
+      number=1, type=8, cpp_type=7, label=2,
+      has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='point_x', full_name='New_client.point_x', index=1,
+      name='point_x', full_name='Login_rsp.point_x', index=1,
       number=2, type=5, cpp_type=1, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='point_y', full_name='New_client.point_y', index=2,
+      name='point_y', full_name='Login_rsp.point_y', index=2,
       number=3, type=5, cpp_type=1, label=2,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='enemy_num', full_name='Login_rsp.enemy_num', index=3,
+      number=4, type=5, cpp_type=1, label=2,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='uid', full_name='Login_rsp.uid', index=4,
+      number=5, type=5, cpp_type=1, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -161,7 +161,56 @@ _NEW_CLIENT = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=117,
-  serialized_end=177,
+  serialized_end=211,
+)
+
+
+_CONNECT_REQ = _descriptor.Descriptor(
+  name='Connect_req',
+  full_name='Connect_req',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  serialized_start=213,
+  serialized_end=226,
+)
+
+
+_CONNECT_RSP = _descriptor.Descriptor(
+  name='Connect_rsp',
+  full_name='Connect_rsp',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='success', full_name='Connect_rsp.success', index=0,
+      number=1, type=8, cpp_type=7, label=2,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  serialized_start=228,
+  serialized_end=258,
 )
 
 
@@ -202,15 +251,16 @@ _ENEMY_MSG = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=179,
-  serialized_end=238,
+  serialized_start=260,
+  serialized_end=319,
 )
 
 DESCRIPTOR.message_types_by_name['Hero_msg'] = _HERO_MSG
 DESCRIPTOR.message_types_by_name['Heart_beat'] = _HEART_BEAT
 DESCRIPTOR.message_types_by_name['Login_req'] = _LOGIN_REQ
 DESCRIPTOR.message_types_by_name['Login_rsp'] = _LOGIN_RSP
-DESCRIPTOR.message_types_by_name['New_client'] = _NEW_CLIENT
+DESCRIPTOR.message_types_by_name['Connect_req'] = _CONNECT_REQ
+DESCRIPTOR.message_types_by_name['Connect_rsp'] = _CONNECT_RSP
 DESCRIPTOR.message_types_by_name['Enemy_msg'] = _ENEMY_MSG
 
 class Hero_msg(_message.Message):
@@ -237,11 +287,17 @@ class Login_rsp(_message.Message):
 
   # @@protoc_insertion_point(class_scope:Login_rsp)
 
-class New_client(_message.Message):
+class Connect_req(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _NEW_CLIENT
+  DESCRIPTOR = _CONNECT_REQ
 
-  # @@protoc_insertion_point(class_scope:New_client)
+  # @@protoc_insertion_point(class_scope:Connect_req)
+
+class Connect_rsp(_message.Message):
+  __metaclass__ = _reflection.GeneratedProtocolMessageType
+  DESCRIPTOR = _CONNECT_RSP
+
+  # @@protoc_insertion_point(class_scope:Connect_rsp)
 
 class Enemy_msg(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
--- window.py
+++ window.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 import time
 import socket
 import threading
+import message_pb2
 
 PIC_X = 1000
 PIC_Y = 600
@@ -28,47 +29,89 @@ BACK_PIC_PATH = "./pic/background.jpg"
 ADDRESS = "localhost"
 PORT = 3080
 
+LOG_REQ='L'
+LOG_RSP='l'
+
+HERO_MSG_REQ="H"
+HERO_MSG_RSP='h'
+
+CONNECT_REQ='C'
+CONNECT_RSP='c'
+
+HEART_REQ='R'
+HEART_REQ='r'
+
+ENEMY_MSG='e'
+
 class HERO_PLAYER(object):
 	"""docstring for player"""
-	def __init__(self, screen):
+	def __init__(self, screen,lock):
 		self.x = 0
 		self.y = 0
+		self.uid = 0
 		self.screen = screen
 		self.image = pygame.image.load(HERO_PIC_PATH);
 		self.up = False
 		self.left = False
 		self.right = False
 		self.down = False
+		self.lock = lock
 
 	def display(self):
-		self.screen.blit(self.image, (self.x,self.y))
+		self.lock.acquire()
+		try:
+			self.screen.blit(self.image, (self.x,self.y))
+		finally:
+			self.lock.release()
 
 	def move_left(self):
-		if self.x >= STEP:
-			self.x -= STEP
+		self.lock.acquire()
+		try:
+			if self.x >= STEP:
+				self.x -= STEP
+		finally:
+			self.lock.release()
 
-	def move_right(self):			
-		if self.x <= X_MAX - STEP:
-			self.x += STEP
+	def move_right(self):	
+		self.lock.acquire()
+		try:		
+			if self.x <= X_MAX - STEP:
+				self.x += STEP
+		finally:
+			self.lock.release()
 
 	def move_up(self):
-		if self.y >= STEP:
-			self.y -= STEP
+		self.lock.acquire()
+		try:
+			if self.y >= STEP:
+				self.y -= STEP
+		finally:
+			self.lock.release()
 
 	def move_down(self):
-		if self.y <= Y_MAX - STEP:
-			self.y += STEP
+		self.lock.acquire()
+		try:
+			if self.y <= Y_MAX - STEP:
+				self.y += STEP
+		finally:
+			self.lock.release()
 
 	def position_update(self):
 		if(self.up == True):
 			self.move_up()
-		if(self.down == True):
+		elif(self.down == True):
 			self.move_down()
-		if(self.left == True):
+		elif(self.left == True):
 			self.move_left()
-		if(self.right == True):
+		elif(self.right == True):
 			self.move_right()	
 
+	def set_uid(self,uid):
+		self.uid = uid
+
+	def set_position(x,y)
+		self.x = x
+		self.y = y
 
 class ENEMY_PLAYER(object):
 	"""docstring for enemy"""
@@ -97,7 +140,19 @@ class ENEMY_PLAYER(object):
 		if self.y <= Y_MAX - STEP:
 			self.y += STEP
 
+class GAME(object):
+	"""docstring for game"""
+	def __init__(self, arg):
+		self.enemy_list = []
+		self.enemy_num = 0
 
+	def append_enemy(self,enemy):
+		self.enemy_list.append(enemy)
+	
+	def set_enemy_num(self,num)
+		self.enemy_num = num
+
+#put to anthor .py file
 def key_control(hero):
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -113,7 +168,7 @@ def key_control(hero):
 				hero.right = True
 
 			elif event.key == K_UP:
-				hero.up = True
+				hero.up = True 
 
 			elif event.key == K_DOWN:
 				hero.down = True
@@ -125,25 +180,150 @@ def key_control(hero):
 			hero.up = False
 
 
-def client_send(sock,hero,enemy_list):
+def login_data_serlia(hero):
+	seli_data = message_pb2.Login_req()
+	seli_data.name = hero.name
+	seli_data.point_x = hero.x
+	seli_data.point_y = hero.y
+	list_ret =[]
+	size = chr(seli_data.ByteSize())
+	list_ret.append(size)
+	list_ret.append(seli_data.SerializeToString()) 
+	return list_ret
+
+def hero_msg_data_serlia(hero):
+	seli_data = message_pb2.Hero_msg()
+	seli_data.uid = hero.uid
+	seli_data.point_x = hero.x
+	seli_data.point_y = hero.y
+
+	list_ret =[]
+	length = seli_data.ByteSize()
+	#print("hero_msg_data_serlia:length=%d"%length)
+	size = chr(seli_data.ByteSize())
+	#print("hero_msg_data_serlia:size=%s"%size)
+	list_ret.append(size)
+	list_ret.append(seli_data.SerializeToString()) 
+	return list_ret
+
+def connect_data_serlia(hero):
+	seli_data = message_pb2.Connect_req()
+
+	list_ret =[]
+	size = chr(seli_data.ByteSize())
+	list_ret.append(size)
+	list_ret.append(seli_data.SerializeToString()) 
+	return list_ret
+
+
+def serialize_data(hero,type):
+	if(type == LOG_REQ):
+		return login_data_serlia(hero)
+
+	elif (type == HERO_MSG_REQ):
+		return hero_msg_data_serlia(hero)
+
+	elif (type == CONNECT_REQ):
+		return connect_data_serlia(hero)
+	
+
+def package_data_send(seria_list,type):
+	type_s = type
+	pack_data = seria_list[0] + type_s + seria_list[1] 
+	#print("len = %s"%pack_data)
+	return pack_data
+
+
+def data_send(sock,pack_data):
+	print pack_data
+	sock.sendall(pack_data)
+
+def client_send(sock,hero,lock):
 	while True:
 		time.sleep(0.1)
-		sock.sendall("hellow tcp server")
-		pass
+		#sock.sendall("hellow tcp server")
+		lock.acquire()  #get lock
+
+		try:
+			seli_data = serialize_data(hero,HERO_MSG_REQ)
+		finally:
+			lock.release()
+
+		pack_data = package_data_send(seli_data,HERO_MSG_REQ)
+		data_send(sock,pack_data)
 
 
 
 def client_recv(sock,buff_recv):
 	while True:
-		data = sock.recv(128)
+		data = sock.recv(1)
 		if data:
-			buff_recv.append(data)
-			print(data)
+			size = ord(data)
+
+		data = sock.recv(1)
+		if data:
+			type = data
+
+		data = sock.recv(size)
+		if(data != size)
+			print("recv err! exit")
+			exit()
+
+		Parse_data(type,data)
+		print(data)
 
 
-def scene_load():
-	pass
 	
+
+def connect_data_parse(data):
+	connect_rsp = message_pb2.Connect_rsp()
+	connect_rsp.ParseFromString(data)
+	return connect_rsp
+
+def enemy_msg_data_parse(data):
+	enemy_msg = message_pb2.Enemy_msg()
+	enemy_msg.ParseFromString(data)
+	return enemy_msg
+
+def login_data_parse(data):
+	login_rsp = message_pb2.Login_rsp()
+	login_rsp.ParseFromString(data)
+	return login_rsp
+
+
+def Parse_data(type,data,buff_recv):
+	if(type == LOG_RSP):
+		return login_data_parse(data)
+
+	elif (type == ENEMY_MSG):
+		return enemy_msg_data_parse(data)
+
+	elif (type == CONNECT_RSP):
+		return connect_data_parse(data)
+
+def log_in(login_rsp,hero,game):
+	if(login_rsp.success == True):
+		hero.set_position(login_rsp.x,login_rsp.y)
+		game.set_enemy_num(login_rsp.enemy_num)
+		hero.set_uid(login_rsp.uid)
+	else:
+		print("log_in err")
+		exit()
+
+def scene_load(hero,game):
+	
+
+
+def handle_game_logic(type,data,hero,game):
+	if(type == LOG_RSP):
+		login_rsp = login_data_parse(data)
+		log_in(login_rsp,hero,game)
+
+	elif (type == ENEMY_MSG):
+		enemy_msg = enemy_msg_data_parse(data)
+
+	elif (type == CONNECT_RSP):
+		connect_rsp = connect_data_parse(data)	
 
 def main():
 
@@ -151,13 +331,15 @@ def main():
 	enemy_list = []
 	screen = pygame.display.set_mode((PIC_X,PIC_Y),0,32)
 	background = pygame.image.load(BACK_PIC_PATH)
-	hero = HERO_PLAYER(screen)
+	lock = threading.Lock()
+	hero = HERO_PLAYER(screen,lock)
 	enemy = ENEMY_PLAYER(screen)
+	game = GAME()
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((ADDRESS, PORT)) 
-
-	print('thread %s is running...' % threading.current_thread().name)
-	thread_read = threading.Thread(target=client_send,args=(sock,hero,enemy_list))
+	
+	print('thread %s is running...' %threading.current_thread().name)
+	thread_read = threading.Thread(target=client_send,args=(sock,hero,lock))
 	thread_write = threading.Thread(target=client_recv,args=(sock,buff_recv))
 	thread_read.start()
 	thread_write.start()
